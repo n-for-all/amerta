29b01b654bbb78d6adec9f97ecd412d344d7a647
Naji Amer <icu090@gmail.com>
2026-02-15 23:03:02 +0400

    Added GTM

--- src/app/(ecommerce)/(default)/layout.tsx
+++ src/app/(ecommerce)/(default)/layout.tsx
@@ -12,6 +12,8 @@ import { Providers } from "@/amerta/providers";
 import { DEFAULT_LOCALE, LOCALES } from "@/amerta/localization/locales";
 import { ThemeShopLayout } from "@/amerta/theme/layout";
 import { getServerSideURL } from "@/amerta/utilities/getURL";
+import { GTM } from "@/amerta/components/GTM/GTM";
+import { GTMFooter } from "@/amerta/components/GTM/GTMFooter";
 
 export default async function RootLayout({ children, locale }: { children: React.ReactNode; locale?: string }) {
   const settings: Settings = await getCachedGlobal("settings" as keyof Config["globals"], 1, locale)();
@@ -25,10 +27,12 @@ export default async function RootLayout({ children, locale }: { children: React
         {settings.appleTouchIcon && typeof settings.appleTouchIcon === "object" && settings.appleTouchIcon.url && <link rel="apple-touch-icon" href={settings.appleTouchIcon.url} sizes="180x180" />}
         {settings.androidIcon && typeof settings.androidIcon === "object" && settings.androidIcon.url && <link rel="icon" href={settings.androidIcon.url} type={settings.androidIcon.url.endsWith(".png") ? "image/png" : settings.androidIcon.url.endsWith(".svg") ? "image/svg+xml" : settings.androidIcon.url.endsWith(".webp") ? "image/webp" : "image/png"} sizes="192x192" />}
         {settings.noIndex && <meta name="robots" content="noindex" />}
+        <GTM />
       </head>
       <body suppressHydrationWarning className="text-black bg-white dark:bg-black dark:text-white">
         <Providers>
           <ThemeShopLayout locale={locale || DEFAULT_LOCALE}>{children}</ThemeShopLayout>
+          <GTMFooter locale={locale || DEFAULT_LOCALE} />
         </Providers>
       </body>
     </html>
